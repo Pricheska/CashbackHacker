0a376e998272a6b85f5fe6227568a2bd93cb9cc2
pricheska <den-ryab2019@yandex.ru>
2023-03-03 17:18:00 +0400

    Make tests on CashBackService.remain() with testNG

--- src/test/java/ru/netology/CashBackServiceTest.java
+++ src/test/java/ru/netology/CashBackServiceTest.java
@@ -3,27 +3,26 @@ package ru.netology;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class CashBackServiceTest {
+    CashBackService service = new CashBackService();
+
 
     @Test
     public void testRemain() {
-        CashBackService service = new CashBackService();
-
         Assert.assertEquals(service.remain(500), 500);
     }
     @Test
-    public void ShouldReturnThousandIfСurrentZero() {
-        CashBackService service = new CashBackService();
-
+    public void ShouldReturnHundred() {
         Assert.assertEquals(service.remain(900), 100);
     }
 
     @Test
     public void ShouldReturnZero() {
-        CashBackService service = new CashBackService();
-
         Assert.assertEquals(service.remain(0), 1000);
     }
+    @Test
+    public void ShouldReturnThousand() {
+        Assert.assertEquals(service.remain(1000), 0);
+    }
+
 }
